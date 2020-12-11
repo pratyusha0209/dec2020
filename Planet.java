96d8a7b78c03769ebdf88cc4409209c3a8963d6d
Pratyusha S A <pratyusha0209@gmail.com>
2020-12-11 12:53:09 +0530

    Java files added

---
+++ Planet.java
@@ -0,0 +1,9 @@
+ class Planet{
+       void revolve(){
+       System.out.println("Revolving");
+       }
+public static void main(String[] args){
+Planet earth=new Planet();
+earth.revolve();
+}
+       }
